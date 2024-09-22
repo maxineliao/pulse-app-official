111e0692e999451704c8c111e0a8340790e87f7f
Maxine <mxnliao@gmail.com>
2024-09-23 00:41:46 +0800

    Environment setup & visitor header created

---
+++ main.js
@@ -0,0 +1,10 @@
+
+import './assets/scss/all.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+import { createIcons, icons } from 'lucide';
+
+// Caution, this will import all the icons and bundle them.
+createIcons({ icons });
