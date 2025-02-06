0e916f3996092540b3e36e5f7146af306927ddb1
Maxine <mxnliao@gmail.com>
2025-02-07 00:50:13 +0800

    Create React Environment

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,11 @@
-import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import { glob } from 'glob';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import liveReload from 'vite-plugin-live-reload';
-
-function moveOutputPlugin() {
-  return {
-    name: 'move-output',
-    enforce: 'post',
-    apply: 'build',
-    async generateBundle(options, bundle) {
-      for (const fileName in bundle) {
-        if (fileName.startsWith('pages/')) {
-          const newFileName = fileName.slice('pages/'.length);
-          bundle[fileName].fileName = newFileName;
-        }
-      }
-    },
-  };
-}
-
+// https://vite.dev/config/
 export default defineConfig({
-  // base 的寫法：
-  // base: '/Repository 的名稱/'
-  base: '/pulse-app-official/',
-  plugins: [
-    liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
-    ViteEjsPlugin(),
-    moveOutputPlugin(),
-  ],
+  base: process.env.NODE_ENV === "production" ? "/pulse-app-official/" : "/",
+  plugins: [react()],
   server: {
-    // 啟動 server 時預設開啟的頁面
-    open: 'pages/index.html',
+    open: 'index.html',
   },
-  build: {
-    rollupOptions: {
-      input: Object.fromEntries(
-        glob
-          .sync('pages/**/*.html')
-          .map((file) => [
-            path.relative('pages', file.slice(0, file.length - path.extname(file).length)),
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
-    },
-    outDir: 'dist',
-  },
-});
+})
