6a46b14ff088f6b4576fd3c8efe890a25ac137c1
fendy <chanes3222@gmail.com>
2025-03-30 21:58:21 +0800

    fixed: player header user NavLink

--- src/components/Player/PlayerHeader.jsx
+++ src/components/Player/PlayerHeader.jsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 export default function PlayerHeader() {
   const navigate = useNavigate();
   const [searchInput, setSearchInput] = useState("");
+  const isAuth = useSelector((state) => state.auth.isAuth);
 
   const handleSearch = (event) => {
     // console.log("searchInput", searchInput);
@@ -61,79 +62,77 @@ export default function PlayerHeader() {
             <List />
           </button>
         </div>
-        <NavLink to="/member_center" className="text-decoration-none">
-          <button className="nav-btn btn btn-outline-primary rounded-3 me-2 pe-lg-2 order-lg-3 d-none d-lg-block">
-            <img
-              className="rounded-circle object-fit-cover me-1"
-              src={PlayerImages.user}
-              alt={decryptedUser.username}
-              width="24"
-              height="24"
+        {isAuth ? (
+          <NavLink to="/member_center" className="text-decoration-none">
+            <button className="nav-btn btn btn-outline-primary rounded-3 me-2 pe-lg-2 order-lg-3 d-none d-lg-block">
+              <img
+                className="rounded-circle object-fit-cover me-1"
+                src={PlayerImages.user}
+                alt={decryptedUser.username}
+                width="24"
+                height="24"
+              />
+              {decryptedUser.username}
+            </button>
+          </NavLink>
+        ) : (
+          <NavLink to="/login" className="text-decoration-none">
+            <button className="nav-btn btn btn-outline-primary rounded-3 me-2 pe-lg-2 order-lg-3 d-none d-lg-block">
+              尚未登入
+            </button>
+          </NavLink>
+        )}
+        <div className="d-lg-none">
+          <div className="container-fluid">
+            <button
+              className="btn rounded-2 player-icon-btn"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasDarkNavbar"
+              aria-controls="offcanvasDarkNavbar"
+              aria-label="Toggle navigation"
+            >
+              <Search />
+            </button>
+            <button className="btn rounded-2 player-icon-btn" type="button">
+              <List />
+            </button>
+            <NavLink to="/member_center" className="text-decoration-none">
+              <button className="btn rounded-2 player-icon-btn" type="button">
+                <User />
+              </button>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      <div
+        className="offcanvas offcanvas-end w-100"
+        tabIndex="-1"
+        id="offcanvasDarkNavbar"
+        aria-labelledby="offcanvasDarkNavbarLabel"
+      >
+        <div className="offcanvas-header">
+          <div className="search-container w-100">
+            <input
+              placeholder="搜尋無損音質歌曲"
+              className="search-input p-0"
+              type="search"
+              aria-label="Search"
             />
-            {decryptedUser.username}
-          </button>
-        </NavLink>
-
-				<div className="d-lg-none">
-					<div className="container-fluid">
-						<button
-							className="btn rounded-2 player-icon-btn"
-							type="button"
-							data-bs-toggle="offcanvas"
-							data-bs-target="#offcanvasDarkNavbar"
-							aria-controls="offcanvasDarkNavbar"
-							aria-label="Toggle navigation"
-						>
-							<Search />
-						</button>
-						<button
-							className="btn rounded-2 player-icon-btn"
-							type="button"
-						>
-							<List />
-						</button>
-						<NavLink
-							to="/member_center"
-							className="text-decoration-none"
-						>
-							<button
-								className="btn rounded-2 player-icon-btn"
-								type="button"
-							>
-								<User />
-							</button>
-						</NavLink>
-					</div>
-				</div>
-			</div>
-			<div
-				className="offcanvas offcanvas-end w-100"
-				tabIndex="-1"
-				id="offcanvasDarkNavbar"
-				aria-labelledby="offcanvasDarkNavbarLabel"
-			>
-				<div className="offcanvas-header">
-					<div className="search-container w-100">
-						<input
-							placeholder="搜尋無損音質歌曲"
-							className="search-input p-0"
-							type="search"
-							aria-label="Search"
-						/>
-						<button className="player-icon-btn btn" type="submit">
-							<Search />
-						</button>
-						<div className="separator"></div>
-						<button
-							type="button"
-							className="btn-close btn-close-white"
-							data-bs-dismiss="offcanvas"
-							aria-label="Close"
-						></button>
-					</div>
-				</div>
-				<div className="offcanvas-body"></div>
-			</div>
-		</header>
-	);
+            <button className="player-icon-btn btn" type="submit">
+              <Search />
+            </button>
+            <div className="separator"></div>
+            <button
+              type="button"
+              className="btn-close btn-close-white"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+        </div>
+        <div className="offcanvas-body"></div>
+      </div>
+    </header>
+  );
 }
